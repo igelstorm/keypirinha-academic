ae1d78bc8b069b2069615903a0033fedad095c6c
Erik Igelström <eigelstrom@gmail.com>
2021-02-05 20:48:33 +0000

    Add ability to open DOI URL in browser

--- academic.py
+++ academic.py
@@ -6,14 +6,9 @@ class Academic(kp.Plugin):
     ITEMCAT_DOI = kp.ItemCategory.USER_BASE + 1
     ITEMCAT_RESULT = kp.ItemCategory.USER_BASE + 1
 
-    ITEMTEXT_PLAINTEXT = {
-        "label": "Copy plaintext reference",
-        "target": "plaintext"
-    }
-    ITEMTEXT_BIBTEX = {
-        "label": "Copy BibTeX reference",
-        "target": "bibtex"
-    }
+    ITEMTEXT_PLAINTEXT = { "label": "Copy plaintext reference", "target": "plaintext" }
+    ITEMTEXT_BIBTEX    = { "label": "Copy BibTeX reference", "target": "bibtex" }
+    ITEMTEXT_URL       = { "label": "Open URL in browser", "target": "url" }
 
     COPYABLE_TARGETS = [
         "plaintext", "bibtex"
@@ -46,21 +41,25 @@ class Academic(kp.Plugin):
 
         plaintext = self.__get_doi(user_input, "text/x-bibliography")
         bibtex = self.__get_doi(user_input, "application/x-bibtex")
+        url = self.__doi_url(user_input)
 
         suggestions = [
+            self.__result_item(bibtex, self.ITEMTEXT_BIBTEX),
             self.__result_item(plaintext, self.ITEMTEXT_PLAINTEXT),
-            self.__result_item(bibtex, self.ITEMTEXT_BIBTEX)
+            self.__result_item(url, self.ITEMTEXT_URL)
         ]
         self.set_suggestions(suggestions, kp.Match.ANY, kp.Sort.NONE)
 
     def on_execute(self, item, action):
         if item.target() in self.COPYABLE_TARGETS:
             kpu.set_clipboard(item.data_bag())
+        if item.target() == "url":
+            kpu.web_browser_command(private_mode=False, url=item.data_bag(), execute=True)
 
     def __get_doi(self, doi, content_type):
         opener = kpnet.build_urllib_opener()
         opener.addheaders = [("Accept", content_type)]
-        with opener.open("https://doi.org/" + doi) as response:
+        with opener.open(self.__doi_url(doi)) as response:
             refce = response.read()
 
         return refce.decode(encoding="utf-8", errors="strict")
@@ -75,3 +74,6 @@ class Academic(kp.Plugin):
                 hit_hint=kp.ItemHitHint.NOARGS,
                 data_bag=content
             )
+
+    def __doi_url(self, doi):
+        return "https://doi.org/" + doi
